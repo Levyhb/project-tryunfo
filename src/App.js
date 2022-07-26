323ef12bfa6e3a714cbb7638de344448d3e6ee46
Levy Bezerra <levybholanda@gmail.com>
2022-07-26 09:35:12 -0300

    Requisito 06 concluído

--- src/App.js
+++ src/App.js
@@ -28,9 +28,9 @@ class App extends React.Component {
     const cardAttr1Int = parseInt(cardAttr1, 10);
     const cardAttr2Int = parseInt(cardAttr2, 10);
     const cardAttr3Int = parseInt(cardAttr3, 10);
+
     const statusIndividual = cardAttr1Int
       <= number90 && cardAttr2Int <= number90 && cardAttr3Int <= number90;
-
     const positive = cardAttr1 >= 0 && cardAttr2 >= 0 && cardAttr3 >= 0;
     const status = cardAttr1Int + cardAttr2Int + cardAttr3Int <= maxStatus;
 
@@ -51,7 +51,30 @@ class App extends React.Component {
     this.setState({ [name]: value }, this.buttonDisabled);
   }
 
-  onSaveButtonClick = () => {
+  onSaveButtonClick = (event) => {
+    event.preventDefault();
+    const { cardName,
+      cardAttr1, cardAttr2, cardAttr3, cardImage,
+      cardDescription, cardRare } = this.state;
+    const card = {
+      name: cardName,
+      attribute1: cardAttr1,
+      attribute2: cardAttr2,
+      attribute3: cardAttr3,
+      image: cardImage,
+      description: cardDescription,
+      rarity: cardRare,
+    };
+
+    this.setState({ cardName: '',
+      cardAttr1: 0,
+      cardAttr2: 0,
+      cardAttr3: 0,
+      cardImage: '',
+      cardDescription: '',
+      cardRare: 'normal',
+    });
+    return card;
   }
 
   render() {
